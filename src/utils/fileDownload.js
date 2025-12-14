8bc2de1a7105f8666674b8bc0967aede2304aff7
mochiyaki <123602593+mochiyaki@users.noreply.github.com>
2025-12-13 18:17:25 -0800

    Add files via upload

---
+++ src/utils/fileDownload.js
@@ -0,0 +1,9 @@
+export function downloadCodeAsFile(code, filename) {
+  const blob = new Blob([code], { type: 'text/plain' })
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = filename
+  a.click()
+  URL.revokeObjectURL(url)
+}
